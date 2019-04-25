19246b461001d428ef6be37cd7b5eb3e0a518694
JaffarShah <sjeffshah@gmail.com>
2019-04-25 13:16:59 -0400

    Create migration files, update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,52 @@
-require('dotenv').config();
+// Update with your config settings.
 
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'migrations'
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   }
 
