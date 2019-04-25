afd013516497841f548e964b8ffa7f43a14a0f1f
Michael Cain <michaelcaincb@gmail.com>
2019-04-25 20:06:28 +0000

    created pg database and migrated tables in pg

--- migrations/20190425131514_users_maps.js
+++ migrations/20190425131514_users_maps.js
@@ -1,16 +1,16 @@
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('users_maps', function (table) {
-        table.increments();
-        table.integer('user_id').unsigned();
-        table.integer('map_id').unsigned();
-        tabple.timestamp('created_at').defaultTo(knex.fn.now())
-        table.timestamp('updated_at').defaultTo(knex.fn.now())
-        table.foreign('user_id').references('id').inTable('users');
-        table.foreign('map_id').references('id').inTable('maps');
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('users_maps', function (table) {
+    table.increments();
+    table.integer('user_id').unsigned();
+    table.integer('map_id').unsigned();
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.foreign('user_id').references('id').inTable('users');
+    table.foreign('map_id').references('id').inTable('maps');
 
-      });
+  });
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('users_maps');
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('users_maps');
 };
